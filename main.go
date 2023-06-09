eab346cba56dfcf3092e235c985ef101796e44d8
yetra <brdujmovic@gmail.com>
2023-06-09 22:24:19 +0200

    Migrate the schema

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"b2match/backend/models"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func main() {
-	_, err := gorm.Open(sqlite.Open("b2match.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("b2match.db"), &gorm.Config{})
 
 	if err != nil {
 		panic("failed to connect to database")
 	}
+
+	// Migrate the schema
+	db.AutoMigrate(&models.Company{})
+	db.AutoMigrate(&models.User{})
+	db.AutoMigrate(&models.Event{})
+	db.AutoMigrate(&models.Meeting{})
+	db.AutoMigrate(&models.Invite{})
 }
