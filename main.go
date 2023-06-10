5fcd2e6a34e2908b73740e75467d39d698d554fd
yetra <brdujmovic@gmail.com>
2023-06-10 12:32:14 +0200

    Add company routes

--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"b2match/backend/database"
+	"b2match/backend/handlers"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	database.SetUpDB()
+
+	route := gin.Default()
+
+	route.GET("/companies", handlers.FindCompanies)
+	route.GET("/companies/:id", handlers.FindCompanyByID)
+	route.POST("/companies", handlers.CreateCompany)
+
+	route.Run(":8085")
 }
