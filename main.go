5757a8f6dc408b8d403ceb3e7326541b2d3c985b
yetra <brdujmovic@gmail.com>
2023-06-10 16:46:07 +0200

    Update routes

--- main.go
+++ main.go
@@ -16,6 +16,8 @@ func main() {
 	route.GET("/companies/:id", handlers.FindCompanyByID)
 	route.POST("/companies", handlers.CreateCompany)
 
+	route.GET("/companies/:id/representatives", handlers.FindCompanyRepresentatives)
+
 	route.GET("/users", handlers.FindUsers)
 	route.GET("/users/:id", handlers.FindUserByID)
 	route.POST("/users", handlers.CreateUser)
@@ -23,6 +25,18 @@ func main() {
 	route.GET("/events", handlers.FindEvents)
 	route.GET("/events/:id", handlers.FindEventByID)
 	route.POST("/events", handlers.CreateUser)
+
+	route.GET("/events/:id/participants", handlers.FindEventParticipants)
+
+	route.POST("/events/:id/join", handlers.JoinEvent)
+
+	route.GET("/events/:id/meetings", handlers.FindEventMeetings)
+	route.POST("/events/:id/meetings", handlers.CreateEventMeeting)
+
+	route.GET("/events/:id/meetings/:meeting_id/invites", handlers.FindMeetingInvites)
+	route.POST("/events/:id/meetings/:meeting_id/invites", handlers.CreateMeetingInvite)
+
+	route.POST("/events/:id/meetings/:meeting_id/invites/:invites_id/rsvp", handlers.RespondToInvite)
 
 	route.Run(":8085")
 }
