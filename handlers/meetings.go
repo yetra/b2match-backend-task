89839cf28adbf16dc31e618dd6c982c802380f25
yetra <brdujmovic@gmail.com>
2023-06-10 16:22:11 +0200

    Add a handler function for creating a Meeting

--- handlers/meetings.go
+++ handlers/meetings.go
@@ -2,12 +2,20 @@ package handlers
 
 import (
 	"net/http"
+	"time"
 
 	"b2match/backend/database"
 	"b2match/backend/models"
 
 	"github.com/gin-gonic/gin"
 )
+
+type newMeetingJSON struct {
+	StartTime time.Time `binding:"required,ltefield=EndTime" json:"start_time" time_format:"2006-01-02"`
+	EndTime   time.Time `binding:"required" json:"end_time" time_format:"2006-01-02"`
+
+	OrganizerID uint `binding:"required" json:"organizer_id"`
+}
 
 // GET /event/:id/meetings
 func FindEventMeetings(c *gin.Context) {
@@ -28,4 +36,43 @@ func FindEventMeetings(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"meetings": meetings})
+}
+
+func CreateEventMeeting(c *gin.Context) {
+	var newMeeting newMeetingJSON
+
+	if err := c.ShouldBindJSON(&newMeeting); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var event models.Event
+
+	id := c.Param("id")
+
+	if err := database.DB.First(&event, id).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var organizer models.User
+
+	if err := database.DB.First(&organizer, newMeeting.OrganizerID).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	meeting := models.Meeting{
+		StartTime:   newMeeting.StartTime,
+		EndTime:     newMeeting.EndTime,
+		EventID:     event.ID,
+		OrganizerID: organizer.ID,
+	}
+
+	if err := database.DB.Create(&meeting).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"meeting": meeting})
 }
