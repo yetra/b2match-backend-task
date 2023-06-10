9855134c41dc6eb21b5ff698f74d8a3bfd2af963
yetra <brdujmovic@gmail.com>
2023-06-10 20:16:42 +0200

    Add binding tags to models

--- models/models.go
+++ models/models.go
@@ -15,7 +15,7 @@ const (
 type Company struct {
 	ID uint `gorm:"primaryKey" json:"id"`
 
-	Name     string `gorm:"not null" json:"name"`
+	Name     string `gorm:"not null" json:"name" binding:"required"`
 	Location string `gorm:"not null" json:"location"`
 	About    string `json:"about"`
 
@@ -25,16 +25,16 @@ type Company struct {
 type User struct {
 	ID uint `gorm:"primaryKey" json:"id"`
 
-	FirstName string `gorm:"not null" json:"first_name"`
-	LastName  string `gorm:"not null" json:"last_name"`
+	FirstName string `gorm:"not null" json:"first_name" binding:"required"`
+	LastName  string `gorm:"not null" json:"last_name" binding:"required"`
 
 	Location string `json:"location"`
 	About    string `json:"about"`
 
-	EMail    string `gorm:"not null;unique" json:"e_mail"`
-	Password string `gorm:"not null" json:"password"`
+	EMail    string `gorm:"not null;unique" json:"e_mail" binding:"required"`
+	Password string `gorm:"not null" json:"password" binding:"required"`
 
-	CompanyID uint `json:"company_id"`
+	CompanyID uint `json:"company_id" binding:"required"`
 
 	OrganizedMeetings []Meeting `gorm:"foreignKey:OrganizerID" json:"organized_meetings"`
 	Invites           []Invite  `gorm:"foreignKey:InviteeID" json:"-"`
@@ -43,12 +43,12 @@ type User struct {
 type Event struct {
 	ID uint `gorm:"primaryKey" json:"id"`
 
-	Name     string `gorm:"not null" json:"name"`
+	Name     string `gorm:"not null" json:"name" binding:"required"`
 	Location string `json:"location"`
 	Agenda   string `json:"agenda"`
 
-	StartDate time.Time `gorm:"not null" json:"start_date"`
-	EndDate   time.Time `gorm:"not null" json:"end_date"`
+	StartDate time.Time `gorm:"not null" json:"start_date" binding:"required,ltefield=EndDate"`
+	EndDate   time.Time `gorm:"not null" json:"end_date" binding:"required"`
 
 	Participants []User    `gorm:"many2many:event_participants;" json:"participants"`
 	Meetings     []Meeting `json:"-"`
@@ -57,13 +57,13 @@ type Event struct {
 type Meeting struct {
 	ID uint `gorm:"primaryKey" json:"id"`
 
-	StartTime time.Time `gorm:"not null" json:"start_time"`
-	EndTime   time.Time `gorm:"not null" json:"end_time"`
+	StartTime time.Time `gorm:"not null" json:"start_time" binding:"required,ltefield=EndTime"`
+	EndTime   time.Time `gorm:"not null" json:"end_time" binding:"required"`
 
 	Scheduled bool `gorm:"not null;default:false" json:"scheduled"`
 
 	EventID     uint `gorm:"not null" json:"event_id"`
-	OrganizerID uint `gorm:"not null" json:"organizer_id"`
+	OrganizerID uint `gorm:"not null" json:"organizer_id" binding:"required"`
 
 	Invites []Invite `json:"-"`
 }
@@ -74,5 +74,5 @@ type Invite struct {
 	Status Status `gorm:"not null;default:0" json:"status"`
 
 	MeetingID uint `gorm:"not null" json:"meeting_id"`
-	InviteeID uint `gorm:"not null" json:"invitee_id"`
+	InviteeID uint `gorm:"not null" json:"invitee_id" binding:"required"`
 }
