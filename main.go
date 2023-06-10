da30f29f9bcf73cf7cd2958340618d97155f784d
yetra <brdujmovic@gmail.com>
2023-06-10 13:28:47 +0200

    Update routes

--- main.go
+++ main.go
@@ -16,5 +16,13 @@ func main() {
 	route.GET("/companies/:id", handlers.FindCompanyByID)
 	route.POST("/companies", handlers.CreateCompany)
 
+	route.GET("/users", handlers.FindUsers)
+	route.GET("/users/:id", handlers.FindUserByID)
+	route.POST("/users", handlers.CreateUser)
+
+	route.GET("/events", handlers.FindEvents)
+	route.GET("/events/:id", handlers.FindEventByID)
+	route.POST("/events", handlers.CreateUser)
+
 	route.Run(":8085")
 }
