533f891af8c968376bcd833ba0d8650965f27d98
yetra <brdujmovic@gmail.com>
2023-06-10 12:26:25 +0200

    Add FindCompanyByID handler function

--- handlers/companies.go
+++ handlers/companies.go
@@ -16,3 +16,17 @@ func FindCompanies(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"companies": companies})
 }
+
+// GET /companies/:id
+func FindCompanyByID(c *gin.Context) {
+	var company models.Company
+
+	id := c.Param("id")
+
+	if err := database.DB.First(&company, id).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"company": company})
+}
