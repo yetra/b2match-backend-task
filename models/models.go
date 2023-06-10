a3eabed6ceb46bae1b6e79e95d11ffabe4a7d16a
yetra <brdujmovic@gmail.com>
2023-06-10 19:24:16 +0200

    Add json tags

--- models/models.go
+++ models/models.go
@@ -13,69 +13,69 @@ const (
 )
 
 type Company struct {
-	ID uint `gorm:"primaryKey"`
+	ID uint `gorm:"primaryKey" json:"id"`
 
-	Name     string
-	Location string
-	About    string
+	Name     string `json:"name"`
+	Location string `json:"location"`
+	About    string `json:"about"`
 
-	Representatives []User
+	Representatives []User `json:"representatives"`
 }
 
 type User struct {
-	ID uint `gorm:"primaryKey"`
+	ID uint `gorm:"primaryKey" json:"id"`
 
-	FirstName string
-	LastName  string
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
 
-	Location string
-	About    string
+	Location string `json:"location"`
+	About    string `json:"about"`
 
-	EMail    string
-	Password string
+	EMail    string `json:"e_mail"`
+	Password string `json:"password"`
 
-	CompanyID uint
+	CompanyID uint `json:"company_id"`
 
-	OrganizedEvents   []Event   `gorm:"foreignKey:OrganizerID"`
-	OrganizedMeetings []Meeting `gorm:"foreignKey:OrganizerID"`
-	Invites           []Invite  `gorm:"foreignKey:InviteeID"`
+	OrganizedEvents   []Event   `gorm:"foreignKey:OrganizerID" json:"organized_events"`
+	OrganizedMeetings []Meeting `gorm:"foreignKey:OrganizerID" json:"organized_meetings"`
+	Invites           []Invite  `gorm:"foreignKey:InviteeID" json:"invites"`
 }
 
 type Event struct {
-	ID uint `gorm:"primaryKey"`
+	ID uint `gorm:"primaryKey" json:"id"`
 
-	Name     string
-	Location string
-	Agenda   string
+	Name     string `json:"name"`
+	Location string `json:"location"`
+	Agenda   string `json:"agenda"`
 
-	StartDate time.Time
-	EndDate   time.Time
+	StartDate time.Time `json:"start_date"`
+	EndDate   time.Time `json:"end_date"`
 
-	OrganizerID uint
+	OrganizerID uint `json:"organizer_id"`
 
-	Participants []User `gorm:"many2many:event_participants;"`
-	Meetings     []Meeting
+	Participants []User    `gorm:"many2many:event_participants;" json:"participants"`
+	Meetings     []Meeting `json:"meetings"`
 }
 
 type Meeting struct {
-	ID uint `gorm:"primaryKey"`
+	ID uint `gorm:"primaryKey" json:"id"`
 
-	StartTime time.Time
-	EndTime   time.Time
+	StartTime time.Time `json:"start_time"`
+	EndTime   time.Time `json:"end_time"`
 
-	Scheduled bool
+	Scheduled bool `json:"scheduled"`
 
-	EventID     uint
-	OrganizerID uint
+	EventID     uint `json:"event_id"`
+	OrganizerID uint `json:"organizer_id"`
 
-	Invites []Invite
+	Invites []Invite `json:"invites"`
 }
 
 type Invite struct {
-	ID uint `gorm:"primaryKey"`
+	ID uint `gorm:"primaryKey" json:"id"`
 
-	Status Status
+	Status Status `json:"status"`
 
-	MeetingID uint
-	InviteeID uint
+	MeetingID uint `json:"meeting_id"`
+	InviteeID uint `json:"invitee_id"`
 }
