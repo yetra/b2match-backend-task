bbf27d99ff0d62692a46a4251dc93a91eb263d02
yetra <brdujmovic@gmail.com>
2023-06-13 00:26:08 +0200

    Add DELETE routes

--- main.go
+++ main.go
@@ -16,11 +16,13 @@ func main() {
 	route.GET("/companies/:id", handlers.GetCompanyByID)
 	route.POST("/companies", handlers.CreateCompany)
 	route.PATCH("/companies/:id", handlers.UpdateCompany)
+	route.DELETE("/companies/:id", handlers.DeleteCompany)
 
 	route.GET("/users", handlers.GetUsers)
 	route.GET("/users/:id", handlers.GetUserByID)
 	route.POST("/users", handlers.CreateUser)
 	route.PATCH("/users/:id", handlers.UpdateUser)
+	route.DELETE("/users/:id", handlers.DeleteUser)
 
 	route.GET("/users/:id/meetings", handlers.GetUserScheduledMeetings)
 	route.GET("/users/:id/invites", handlers.GetUserInvites)
@@ -29,6 +31,7 @@ func main() {
 	route.GET("/events/:id", handlers.GetEventByID)
 	route.POST("/events", handlers.CreateEvent)
 	route.PATCH("/events/:id", handlers.UpdateEvent)
+	route.DELETE("/events/:id", handlers.DeleteEvent)
 
 	route.POST("/events/:id/join", handlers.JoinEvent)
 
@@ -37,12 +40,14 @@ func main() {
 
 	route.GET("/meetings/:id", handlers.GetMeetingByID)
 	route.PATCH("/meetings/:id/schedule", handlers.ScheduleMeeting)
+	route.DELETE("/meetings/:id", handlers.DeleteMeeting)
 
 	route.GET("/meetings/:id/invites", handlers.GetMeetingInvites)
 	route.POST("/meetings/:id/invites", handlers.CreateMeetingInvite)
 
 	route.GET("/invites/:id", handlers.GetInviteByID)
 	route.PATCH("/invites/:id/rsvp", handlers.RespondToInvite)
+	route.DELETE("/invites/:id", handlers.DeleteInvite)
 
 	route.Run(":8085")
 }
