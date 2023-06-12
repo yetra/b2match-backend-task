1086e9252a35a104753bf0fbbd886d0de60936e5
yetra <brdujmovic@gmail.com>
2023-06-12 17:57:54 +0200

    Refactor using getTypeName

--- handlers/base.go
+++ handlers/base.go
@@ -14,11 +14,21 @@ type resource interface {
 	models.Company | models.User | models.Event | models.Meeting | models.Invite
 }
 
+func getTypeName(variable interface{}) string {
+	t := reflect.TypeOf(variable)
+
+	if t.Kind() == reflect.Slice || t.Kind() == reflect.Ptr {
+		return t.Elem().Name()
+	} else {
+		return t.Name()
+	}
+}
+
 func findResources[R resource](c *gin.Context) {
 	var resources []R
 	database.DB.Find(&resources)
 
-	resourcesName := strings.ToLower(reflect.TypeOf(resources).Elem().Name())
+	resourcesName := strings.ToLower(getTypeName(resources))
 
 	c.JSON(http.StatusOK, gin.H{resourcesName: resources})
 }
