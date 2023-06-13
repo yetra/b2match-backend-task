46989f719f4f3c005ec8c783fa432a39b7c3afbc
yetra <brdujmovic@gmail.com>
2023-06-13 16:21:05 +0200

    Set up Swagger docs in main.go

--- main.go
+++ main.go
@@ -2,18 +2,36 @@ package main
 
 import (
 	"b2match/backend/database"
+	"b2match/backend/docs"
 	"b2match/backend/routes"
+
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
-// Main sets up the b2match SQLite database, creates a router, and starts the server.
+func setUpSwagger(router *gin.Engine) {
+	docs.SwaggerInfo.Title = "b2match API"
+	docs.SwaggerInfo.Description = "An API for event management."
+	docs.SwaggerInfo.Version = "1.0"
+
+	docs.SwaggerInfo.Host = "localhost:8085"
+	docs.SwaggerInfo.BasePath = "/"
+	docs.SwaggerInfo.Schemes = []string{"http"}
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+}
+
 func main() {
 	database.SetUpDB("b2match.db", &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
 
 	router := routes.CreateRouter()
+	setUpSwagger(router)
+
 	router.Run(":8085")
 }
